70c119d26bfa9d4e99404240e3a72a7b5b038cff
Alexandre Morais <piekatoski2001@gmail.com>
2021-08-18 13:51:52 -0300

    Calculadora em Java

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        double n1, n2;
+        double soma, subtracao, multiplicacao, divisao;
+        int op;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro valor:");
+        n1 = entrada.nextDouble();
+        System.out.println("Informe o segundo valor:");
+        n2 = entrada.nextDouble();
+
+        System.out.println("######SELECIONE UMA OPERAÇÃO######");
+        System.out.println("[1] - SOMAR");
+        System.out.println("[2] - SUBTRAIR");
+        System.out.println("[3] - MULTIPLICAR");
+        System.out.println("[4] - DIVIDIR");
+        System.out.println(">>>>>>>>>>>>>>>>>>Digite sua opção:<<<<<<<<<<<<<<<<<<<n ");
+        op = entrada.nextInt();
+        switch (op) {
+            case 1:
+                soma = n1 + n2;
+                System.out.println("A soma é:" + soma);
+                break;
+
+            case 2:
+                subtracao = n1 - n2;
+                System.out.println("A subtração é:" + subtracao);
+                break;
+
+            case 3:
+                multiplicacao = n1 * n2;
+                System.out.println("A multiplicação é: " + multiplicacao);
+                break;
+
+            case 4:
+                if (n1 < n2) {
+                    System.out.println("Impossivel realizar o cálculo");
+                } else {
+                    divisao = n1 / n2;
+                    System.out.println("A divisao é: " + divisao);
+                }
+                break;
+
+            default:
+                System.out.println("OPERAÇÃO INVÁLIDA!!");
+        }
+    }
+}
